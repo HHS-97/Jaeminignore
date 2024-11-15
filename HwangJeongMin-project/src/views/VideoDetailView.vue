29f3c04324d6ffe038f2c591bfb059c2570b885f
jeongin <2ni00@naver.com>
2024-11-15 14:29:59 +0900

    상세정보

--- HwangJeongMin-project/src/views/VideoDetailView.vue
+++ HwangJeongMin-project/src/views/VideoDetailView.vue
@@ -1,10 +1,25 @@
 <template>
-    <div>
-
-    </div>
+  <h1>{{  }}</h1>
+  <p>{{  }}</p>
+  <iframe src="" frameborder="0"></iframe>
+  <p>{{  }}</p>
+  <div>
+    <button class="btn btn-primary">동영상 저장</button>
+  </div>
+  <div>
+    <button class="btn btn-primary">저장 취소</button>
+  </div>
 </template>
 
 <script setup>
+import { useRoute } from 'vue-router';
+import { useVideoStore } from '@/stores/video';
+
+const store = useVideoStore()
+const route = useRoute()
+// const videoId = 
+
+// const video = 
 
 </script>
 
